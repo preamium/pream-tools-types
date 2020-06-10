7dbab34230d65302743e6dbadd62b18b651ddaa4
laurent.godet <laurent.godet@masternaut.com>
2020-06-10 18:21:45 +0200

    add index

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+import { IApp } from './app.interface';
+import { IChannel } from './channel.interface';
+import { ISite } from './site.interface';
+import { IStream } from './stream.interface';
+import { IRenderInput, IRenderOutput } from './io';
+export { IApp, IChannel, ISite, IStream, IRenderInput, IRenderOutput };
---
+++ src/interfaces/index.ts
@@ -0,0 +1,8 @@
+import { IApp } from './app.interface'
+import { IChannel } from './channel.interface'
+import { ISite } from './site.interface'
+import { IStream } from './stream.interface'
+import { IRenderInput, IRenderOutput } from './io'
+
+
+export { IApp, IChannel, ISite, IStream, IRenderInput, IRenderOutput }
