d7f0175ff78ea6792987eaa6b46f29f15ab1997e
laurent.godet <laurent.godet@masternaut.com>
2020-08-31 22:51:23 +0200

    added template to channel

--- dist/models/channel.interface.d.ts
+++ dist/models/channel.interface.d.ts
@@ -1,6 +1,7 @@
 import * as mongoose from 'mongoose';
 export interface IChannel extends mongoose.Document {
     name: string;
+    template: string;
     stacks: {
         id: string;
         params: [{
