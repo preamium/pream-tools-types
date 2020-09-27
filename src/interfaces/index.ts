c955b10a1251f36d68c03b24d9c3818bb09fcdce
laurent.godet <laurent.godet@masternaut.com>
2020-09-27 15:26:44 +0200

    added export of template interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,6 +1,7 @@
 import { IApp } from './models/app.interface'
 import { IChannel } from './models/channel.interface'
 import { ISite } from './models/site.interface'
+import { ITemplate } from './models/template.interface'
 import { IStream } from './models/stream.interface'
 import { IRenderInput } from './render/render-input.interface'
 import { IRenderOutput } from './render/render-output.interface'
@@ -8,4 +9,4 @@ import { IScreenResponse } from './screen/screen-response.interface'
 import { IAppRequestPayload } from './app/app-request-payload.interface'
 import { IAppResponsePayload } from './app/app-response-payload.interface'
 
-export { IApp, IChannel, ISite, IStream, IRenderInput, IRenderOutput, IScreenResponse, IAppRequestPayload, IAppResponsePayload }
+export { ITemplate, IApp, IChannel, ISite, IStream, IRenderInput, IRenderOutput, IScreenResponse, IAppRequestPayload, IAppResponsePayload }
